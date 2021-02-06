7b76306faae2f2be9d9c6475461fdb52f356b8c7
perth98 <choongdo@hotmail.com>
2021-02-06 19:52:15 +0900

    33

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,31 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Habit extends Component {
-    handleIncrement = (habit) => {
-        // this.props.
-    }
+  handleIncrement = (habit) => {
+    // this.props.
+  };
 
-    render () {
-        return (
-            <li className="habit">
-              <span className="habit-name">Reading</span>
-              <span className="habit-count">8</span>
-              <button 
-                class="habit-button habit-increase"
-                onClick={this.handleIncrement}
-              >
-                  <i className="fas fa-plus-square"></i>
-              </button>
-              <button class="habit-button habit-decrease">
-                  <i className="fas fa-minus-square"></i>
-              </button>
-              <button class="habit-button habit-delete">
-                  <i className="fas fa-trash"></i>
-              </button>
-              <button>test</button>
-            </li>
-          );
-    };
+  render() {
+    return (
+      <li className="habit">
+        <span className="habit-name">Reading</span>
+        <span className="habit-count">8</span>
+        <button
+          class="habit-button habit-increase"
+          onClick={this.handleIncrement}
+        >
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <button class="habit-button habit-decrease">
+          <i className="fas fa-minus-square"></i>
+        </button>
+        <button class="habit-button habit-delete">
+          <i className="fas fa-trash"></i>
+        </button>
+        <button>test33</button>
+      </li>
+    );
+  }
 }
 
-export default Habit
+export default Habit;

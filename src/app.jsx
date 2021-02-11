ff4b1cfd9e69173dd96df5b1017338b72d4b1659
perth98 <choongdo@hotmail.com>
2021-02-11 23:45:30 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,67 +1,22 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import './app.css';
-import Habits from './components/habits';
-import Navbar from './components/navbar';
+import VideoList from './components/video_list/video_list';
 
-class App extends Component {
-  state = {
-    habits: [
-      { id: 1, name: 'Reading', count: 0 },
-      { id: 2, name: 'Running', count: 0 },
-      { id: 3, name: 'Coding', count: 0 },
-    ],
-  };
+function App() {
+  const [videos, setVideos] = useState([]);
 
-  handleIncrement = habit => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    habits[index].count++;
-    this.setState({ habit });
-  };
-
-  handleDecrement = habit => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    const count = habits[index].count - 1;
-    habits[index].count = count < 0 ? 0 : count;
-    this.setState({ habit });
-  };
-
-  handleDelete = habit => {
-    const habits = this.state.habits.filter(item => item.id !== habit.id);
-    this.setState({ habits });
-  };
-
-  handleAdd = name => {
-    const habits = [...this.state.habits, { id: Date.now(), name, count: 0 }];
-    this.setState({ habits });
-  };
-
-  handleReset = () => {
-    const habits = this.state.habits.map(habit => {
-      habit.count = 0;
-      return habit;
-    });
-    this.setState({ habits });
-  };
-
-  render() {
-    return (
-      <>
-        <Navbar
-          totalCount={this.state.habits.filter(item => item.count > 0).length}
-        />
-        <Habits
-          habits={this.state.habits}
-          onIncrement={this.handleIncrement}
-          onDecrement={this.handleDecrement}
-          onDelete={this.handleDelete}
-          onAdd={this.handleAdd}
-          onReset={this.handleReset}
-        />
-      </>
-    );
-  }
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBOJkUoWoYTVuxNO6pqC41a840tlwoRtI0", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+  return <VideoList videos={videos} />
 }
 
 export default App;
